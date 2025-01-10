76077e6dadb9e46c5579c79355feb5eaba42df03
SKX <stormraida01@gmail.com>
2025-01-10 14:01:13 -0500

    test 0.0.1.6

--- script/RedEyes/c220000009.lua
+++ script/RedEyes/c220000009.lua
@@ -13,34 +13,31 @@ function s.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetValue(CARD_REDEYES_B_DRAGON)
 	c:RegisterEffect(e1)
-	--[[
-	If this card is the monster with the highest ATK on the field (even if it's tied), it can attack thrice per Battle Phase.
-	Other monsters you control cannot declare an attack during the turn this card declares 2 or more attacks.
-	]]--
+	-- If this card is the monster with the highest ATK on the field (even if it's tied), it can attack thrice per Battle Phase.
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_EXTRA_ATTACK)
-	e2:SetCost(s.e2cst)
 	e2:SetCondition(s.e2con)
 	e2:SetValue(2)
 	c:RegisterEffect(e2)
-	--[[
-	local e2b=Effect.CreateEffect(c)
-	e2b:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e2b:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e2b:SetTarget(s.e2btgt)
-	e2b:SetOperation(s.e2bevt)
-	c:RegisterEffect(e2b)
-	]]--
+	-- Other monsters you control cannot declare an attack during the turn this card declares 2 or more attacks.
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetCondition(s.e3con)
+	e3:SetTarget(s.e3tgt)
+	c:RegisterEffect(e3)
 	-- Negate the effect of any card that would increase the ATK of a monster your opponent controls.
 	--[[
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_CHAIN_SOLVING)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(s.e3con)
-	e3:SetOperation(s.e3evt)
-	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_CHAIN_SOLVING)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(s.e4con)
+	e4:SetOperation(s.e4evt)
+	c:RegisterEffect(e4)
 	aux.DoubleSnareValidity(c,LOCATION_MZONE)
 	]]--
 end
@@ -52,56 +49,27 @@ s.listed_series={SET_RED_EYES}
 function s.e2fil(c,ec)
 	return c==ec
 end
-function s.e2lim(e,c)
-	return e:GetLabel()~=c:GetFieldID()
-end
-function s.e2cst(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return true end
-
-	local e2c=Effect.CreateEffect(c)
-	e2c:SetType(EFFECT_TYPE_FIELD)
-	e2c:SetProperty(EFFECT_FLAG_OATH)
-	e2c:SetCode(EFFECT_CANNOT_ATTACK)
-	e2c:SetTargetRange(LOCATION_MZONE,0)
-	e2c:SetTarget(s.e2lim)
-	e2c:SetLabel(c:GetFieldID())
-	e2c:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2c,tp)
-end
 function s.e2con(e)
 	local c=e:GetHandler()
 	local tp=c:GetControler()
 
 	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil):GetMaxGroup(Card.GetAttack)
+	
 	return g and g:IsExists(s.e2fil,1,nil,c)
 end
-function s.e2btgt(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then
-		return c:GetAttackAnnouncedCount()==2
-	end
+function s.e3con(e)
+	return e:GetHandler():GetAttackAnnouncedCount()>=2
 end
-function s.e2bevt(e,tp)
-	local c=e:GetHandler()
-
-	local e2c=Effect.CreateEffect(c)
-	e2c:SetType(EFFECT_TYPE_FIELD)
-	e2c:SetCode(EFFECT_CANNOT_ATTACK)
-	e2c:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_OATH)
-	e2c:SetTargetRange(LOCATION_MZONE,0)
-	e2c:SetTarget(s.e2lim)
-	e2c:SetLabel(c:GetFieldID())
-	e2c:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2c,tp)
+function s.e3tgt(e,c)
+	return c~=e:GetHandler()
 end
-function s.e3con(e,tp,eg,ep,ev,re,r,rp)
+function s.e4con(e,tp,eg,ep,ev,re,r,rp)
 	local p,o=re:GetTargetRange()
 
 	return re:IsHasCategory(CATEGORY_ATKCHANGE)
 	and re:GetValue()>0
 	and o~=0
 end
-function s.e3evt(e,tp,eg,ep,ev)
+function s.e4evt(e,tp,eg,ep,ev)
 	Duel.NegateEffect(ev)
 end
