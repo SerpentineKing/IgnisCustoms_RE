447c0091641f9422a4b589a3dcc973bd7f20414d
SKX <stormraida01@gmail.com>
2025-01-22 00:56:57 -0500

    test 0.1.3.0.b [f3]

--- script/RedEyes/c220000039.lua
+++ script/RedEyes/c220000039.lua
@@ -74,8 +74,8 @@ function s.e1evt(e,tp)
 	e1b:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1b,tp)
 end
-function s.e1bval(e,re)
-	return re:GetHandler():GetAttack()/2
+function s.e1bval(e,c)
+	return c:GetAttack()/2
 end
 function s.e2val(e,re,r)
 	if (r&REASON_BATTLE+REASON_EFFECT)~=0 then
