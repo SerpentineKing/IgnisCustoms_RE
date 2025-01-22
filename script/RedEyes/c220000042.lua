f84b60e35696649411be70dc9317ae0adb32bbe3
SKX <stormraida01@gmail.com>
2025-01-22 13:39:05 -0500

    debug 0.1.4.0.b [f3.6]

--- script/RedEyes/c220000042.lua
+++ script/RedEyes/c220000042.lua
@@ -61,7 +61,7 @@ function s.e1con(e,tp,eg,ep,ev,re,r,rp)
 	local req=false
 	if ch>=1 then
 		local ch_ev=ch-1
-		local ch_p,ch_e=Duel.GetChainInfo(tev,CHAININFO_TRIGGERING_PLAYER,CHAININFO_TRIGGERING_EFFECT)
+		local ch_p,ch_e=Duel.GetChainInfo(ch_ev,CHAININFO_TRIGGERING_PLAYER,CHAININFO_TRIGGERING_EFFECT)
 
 		if (ch_e:GetCode()==EVENT_SUMMON_SUCCESS or ch_e:GetCode()==EVENT_SPSUMMON_SUCCESS) and Duel.IsChainNegatable(ch_ev) and ch_p~=tp then
 			req=true
