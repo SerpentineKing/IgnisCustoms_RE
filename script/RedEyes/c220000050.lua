c8d8099f0d734dd5580857b56a98273988860303
SKX <stormraida01@gmail.com>
2025-04-07 22:35:32 -0400

    test 1.6.3.0.a

--- script/RedEyes/c220000050.lua
+++ script/RedEyes/c220000050.lua
@@ -3,13 +3,16 @@ local s,id,o=GetID()
 -- c220000050
 function s.initial_effect(c)
 	-- "Time Wizard" + 1 Level 3 or 4 Dragon monster
-	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsCode,71625222),s.m2fil)
+	Fusion.AddProcMix(c,true,true,s.m1fil,s.m2fil)
 	c:EnableReviveLimit()
 end
 -- Mentions : "Time Wizard"
 s.listed_names={71625222,id}
 -- Archetype : N/A
 -- Helpers
+function s.m1fil(c,fc,sumtype,tp)
+	return c:IsCode(71625222)
+end
 function s.m2fil(c,fc,sumtype,tp)
 	return c:IsRace(RACE_DRAGON)
 	and (c:IsLevel(3) or c:IsLevel(4))
