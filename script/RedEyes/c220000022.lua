f34e428662ff8d0a35418162fd85dcdd54dae46a
SerpentineKing <stormraida01@gmail.com>
2026-06-09 20:53:26 -0400

    test 0.2.0.2.f [1.0]

--- script/RedEyes/c220000022.lua
+++ script/RedEyes/c220000022.lua
@@ -94,6 +94,8 @@ function s.e2tgt(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 end
 function s.e2evt(e,tp)
+	local c=e:GetHandler()
+	
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
 
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
